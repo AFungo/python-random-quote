079245675de0b34313d0848d59aa445d42345bec
AFungo <augustofungo@gmail.com>
2022-08-23 11:43:39 -0300

    I do the last issue

--- get-quote.py
+++ get-quote.py
@@ -3,14 +3,20 @@ import random
 def primary():
 
 
+  f = open("quotes.txt", "a")
+  f.write("\nadd new random " + str(random.randint(1, 100)))
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
-  last = len(quotes)-1
-  rnd = random.randint(0, last)
   
-  print(quotes[rnd])
 
+  last = len(quotes)-1
+
+  
+  for i in range(3):
+    rnd = random.randint(0, last)
+    print(quotes[rnd].strip())
+    
 if __name__== "__main__":
   primary()
